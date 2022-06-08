d922fd6a0e87bd8bad69ce30b7c5c61af69ab745
Surya Prakash <suryap@tabtale.com>
2022-06-08 16:41:11 +0530

    Cheese Cut with fall

--- SayCheese/Assets/SayCheese/_Scripts/Level/SliceController.cs
+++ SayCheese/Assets/SayCheese/_Scripts/Level/SliceController.cs
@@ -5,36 +5,63 @@ using NaughtyAttributes;
 using BzKovSoft.ObjectSlicer.Samples;
 using Deform;
 
+public enum Direction
+{
+    Up, Down, Left, Right, Forward, Back
+}
 public class SliceController : MonoBehaviour
 {
+    BendDeformer currentBendDeformer;
+
+    [SerializeField] private Direction direction;
+
+    GameObject currentCutObject;
+    public GameObject levelObject;
     public GameObject knife;
+
     public CapsuleCollider knifeCollider;
+
     public BendDeformer knifeDeformer;
+
     Vector3 startPos;
+    Vector3 moveDir;
 
     public float knifeDownLimit = 0.2f;
-    public float knifeSpeed;
+    public float knifeSpeed = 2f;
+    public float levelObjectSpeed = 1f;
 
     bool mouseHeld;
-    bool knifeReached;
+    bool cutting;
+
+    void OnEnable()
+    {
+        Sliceable.sliced += OnSliced;
+    }
+
+    void OnDisable()
+    {
+        Sliceable.sliced -= OnSliced;
+    }
 
     void Start()
     {
         startPos = knife.transform.position;
+        AssingMoveDir();
     }
     public void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            OnMouseButtonDown(); 
+            OnMouseButtonDown();
         }
 
-        if(Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0))
         {
             OnMouseButtonUp();
         }
 
         KnifeMovement();
+        LevelObjectMovement();
     }
 
 
@@ -50,18 +77,73 @@ public class SliceController : MonoBehaviour
         mouseHeld = false;
     }
 
+    void AssingMoveDir()
+    {
+        switch (direction)
+        {
+            case Direction.Up:
+                moveDir = levelObject.transform.up;
+                break;
+            case Direction.Down:
+                moveDir = -levelObject.transform.up;
+                break;
+            case Direction.Left:
+                moveDir = -levelObject.transform.right;
+                break;
+            case Direction.Right:
+                moveDir = levelObject.transform.right;
+                break;
+            case Direction.Forward:
+                moveDir = levelObject.transform.forward;
+                break;
+            case Direction.Back:
+                moveDir = -levelObject.transform.forward;
+                break;
+        }
+    }
+    void LevelObjectMovement()
+    {
+        if (mouseHeld) return;
+        if (cutting) return;
+        levelObject.transform.position += moveDir * Time.deltaTime * levelObjectSpeed;
+    }
+
     void KnifeMovement()
     {
-        if(!mouseHeld) return;
+        if (!mouseHeld) return;
         float posy = knife.transform.position.y;
         posy -= Time.deltaTime * knifeSpeed;
-        if(posy <= knifeDownLimit)
+        if (posy <= knifeDownLimit)
         {
-            knifeReached = true;
             posy = knifeDownLimit;
-            mouseHeld = false;
-            LerpObjectPosition.instance.LerpObject(knife.transform, startPos, 0.5f);
+            KnifeReached();
         }
         knife.transform.position = new Vector3(knife.transform.position.x, posy, knife.transform.position.z);
+    }
+
+    void KnifeReached()
+    {
+        mouseHeld = false;
+        LerpObjectPosition.instance.LerpObject(knife.transform, startPos, 0.2f);
+
+        if (currentBendDeformer == null) return;
+
+        currentBendDeformer.transform.parent = currentCutObject.transform;
+        currentCutObject.GetComponent<Rigidbody>().isKinematic = false;
+
+        cutting = false;
+    }
+
+    void OnSliced(GameObject cutObject)
+    {
+        cutting = true;
+        BendDeformer bendDeformer = Instantiate(knifeDeformer, knifeDeformer.transform);
+        bendDeformer.transform.localPosition = Vector3.zero;
+        bendDeformer.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        bendDeformer.transform.localScale = Vector3.one;
+        Deformable deform = cutObject.AddComponent<Deformable>();
+        deform.AddDeformer(bendDeformer);
+        currentCutObject = cutObject;
+        currentBendDeformer = bendDeformer;
     }
 }
--- SayCheese/Assets/SayCheese/_Scripts/Level/Sliceable.cs
+++ SayCheese/Assets/SayCheese/_Scripts/Level/Sliceable.cs
@@ -1,52 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BzKovSoft.ObjectSlicer;
 using BzKovSoft.ObjectSlicer.Samples;
 
 [DisallowMultipleComponent]
 public class Sliceable : MonoBehaviour
 {
     IBzSliceableNoRepeat _sliceableAsync;
+    public static System.Action<GameObject> sliced;
+    void Start()
+    {
+        _sliceableAsync = GetComponentInParent<IBzSliceableNoRepeat>();
+    }
 
-        void Start()
+    void OnTriggerEnter(Collider other)
+    {
+        var knife = other.gameObject.GetComponent<BzKnife>();
+        if (knife == null)
+            return;
+
+        StartCoroutine(Slice(knife));
+    }
+
+    private IEnumerator Slice(BzKnife knife)
+    {
+        // The call from OnTriggerEnter, so some object positions are wrong.
+        // We have to wait for next frame to work with correct values
+        yield return null;
+
+        Vector3 point = GetCollisionPoint(knife);
+        Vector3 normal = Vector3.Cross(knife.MoveDirection, knife.BladeDirection);
+        Plane plane = new Plane(normal, point);
+
+        if (_sliceableAsync != null)
         {
-            _sliceableAsync = GetComponentInParent<IBzSliceableNoRepeat>();
-        }
-
-        void OnTriggerEnter(Collider other)
-        {
-            var knife = other.gameObject.GetComponent<BzKnife>();
-            if (knife == null)
-                return;
-
-            StartCoroutine(Slice(knife));
-        }
-
-        private IEnumerator Slice(BzKnife knife)
-        {
-            // The call from OnTriggerEnter, so some object positions are wrong.
-            // We have to wait for next frame to work with correct values
-            yield return null;
-
-            Vector3 point = GetCollisionPoint(knife);
-            Vector3 normal = Vector3.Cross(knife.MoveDirection, knife.BladeDirection);
-            Plane plane = new Plane(normal, point);
-
-            if (_sliceableAsync != null)
+            _sliceableAsync.Slice(plane, knife.SliceID, (BzSliceTryResult result) =>
             {
-                _sliceableAsync.Slice(plane, knife.SliceID, null);
-                print("Cut");
-
-            }
+                if (!result.sliced) return;
+                sliced?.Invoke(result.outObjectPos);
+            });
+            knife.enabled = false;
         }
+    }
 
-        private Vector3 GetCollisionPoint(BzKnife knife)
-        {
-            Vector3 distToObject = transform.position - knife.Origin;
-            Vector3 proj = Vector3.Project(distToObject, knife.BladeDirection);
+    private Vector3 GetCollisionPoint(BzKnife knife)
+    {
+        Vector3 distToObject = transform.position - knife.Origin;
+        Vector3 proj = Vector3.Project(distToObject, knife.BladeDirection);
 
-            Vector3 collisionPoint = knife.Origin + proj;
-            return collisionPoint;
-        }
+        Vector3 collisionPoint = knife.Origin + proj;
+        return collisionPoint;
+    }
 
 }
